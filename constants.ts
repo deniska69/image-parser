89c5e078e1e7d32006467030087b892cd87b2221
deniska69 <gostev.dy@yandex.ru>
2025-04-04 02:39:55 +0700

    complete parse href

---
+++ constants.ts
@@ -0,0 +1,1 @@
+export const SELECTOR = 'a.sku__slides-item > img';
--- index.ts
+++ index.ts
@@ -1,25 +1,48 @@
 import * as fs from 'fs';
+import axios from 'axios';
+import * as cheerio from 'cheerio';
 import { getUrl, readFile, getTitle } from './helpers.js';
-
-console.log('');
-console.log('------------- START -------------');
-console.log('');
+import { SELECTOR } from './constants.js';
 
 const file = readFile();
 
-console.log(0, getTitle(file, 0));
-console.log(getUrl(file, 0), '\n');
-console.log(1, getTitle(file, 1));
-console.log(getUrl(file, 1), '\n');
-console.log(2, getTitle(file, 2));
-console.log(getUrl(file, 2));
-
-console.log('');
-console.log('------------- END -------------');
-console.log('');
-
 if (!fs.existsSync('images')) fs.mkdirSync('images');
 
-const downdloads = () => {};
+const downdloads = async () => {
+	console.log('----- START -----');
+
+	const a = 0;
+	const b = 1;
+	const c = 91;
+
+	const n = c;
+	const url = getUrl(file, n);
+
+	if (!url) return console.error('url is undefined');
+
+	console.log('---------------');
+	console.log(n, getTitle(file, n));
+	console.log(getUrl(file, n));
+	console.log('---------------');
+
+	await axios
+		.get(url)
+		.then(({ data }) => {
+			const $ = cheerio.load(data);
+
+			const el = $(SELECTOR).attr();
+
+			const dataLazy = el && 'data-lazy' in el ? el['data-lazy'] : '';
+			const href = !dataLazy.includes('placeholder') ? dataLazy : undefined;
+
+			if (href) {
+				console.log({ href });
+			} else {
+				console.log('NO PHOTO');
+			}
+		})
+		.catch((e) => console.error(e))
+		.finally(() => console.log('----- END -----'));
+};
 
 downdloads();
