736d806f3ca7642720c55534ecccc95f898a6c68
deniska69 <gostev.dy@yandex.ru>
2025-04-04 03:52:56 +0700

    completed

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import * as fs from 'fs';
 import { inferSchema, initParser } from 'udsv';
 
@@ -9,14 +10,6 @@ export const readFile = (): string[][] => {
 	return parsedFile;
 };
 
-export const getTitle = (file: string[][], row: number) => {
-	return file[row]?.length ? file[row][0] : undefined;
-};
-
-export const getUrl = (file: string[][], row: number) => {
-	return file[row]?.length ? file[row][1] : undefined;
-};
-
 export const sleep = (milliseconds = 1000) => {
 	console.info('---sleep---');
 	const date = Date.now();
@@ -24,4 +17,37 @@ export const sleep = (milliseconds = 1000) => {
 	do {
 		currentDate = Date.now();
 	} while (currentDate - date < milliseconds);
+};
+
+export const getHref = (string: string) => {
+	return !string.includes('placeholder') && string.includes('https://') ? string : undefined;
+};
+
+export const download = async (title: string, url: string) => {
+	try {
+		const filepath = `images/${title}.jpg`;
+
+		if (fs.existsSync(filepath)) {
+			console.log('\x1b[31m%s\x1b[0m', 'The image already exists');
+			return Promise.resolve();
+		}
+
+		sleep();
+
+		const response = await axios({
+			url,
+			method: 'GET',
+			responseType: 'stream',
+		});
+
+		return await new Promise((resolve, reject) => {
+			response.data
+				.pipe(fs.createWriteStream(filepath))
+				.on('error', reject)
+				.once('close', () => resolve(filepath));
+		});
+	} catch (e) {
+		console.log('\x1b[31m%s\x1b[0m', 'download() error #1:');
+		console.error(e);
+	}
 };
--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 import axios from 'axios';
 import * as cheerio from 'cheerio';
-import { getUrl, readFile, getTitle, sleep } from './helpers.js';
+import { readFile, sleep, getHref, download } from './helpers.js';
 import { SELECTOR } from './constants.js';
 
 const file = readFile();
@@ -11,41 +11,52 @@ if (!fs.existsSync('images')) fs.mkdirSync('images');
 const downdloads = async () => {
 	console.log('----- START -----');
 
-	const array = [0, 1, 2, 91];
+	let total = 0;
+	let exist = 0;
+	let errors = 0;
+	let errorsArray: string[] = [];
 
-	for (const item of array) {
-		const title = getTitle(file, item);
-		const url = getUrl(file, item);
+	for (const item of file) {
+		total++;
+
+		const title = item[0];
+		const url = item[1];
 
 		console.log('');
+		console.log(total, '/', file.length);
 		console.log('title:', title);
 		console.log('url:', url);
 
-		if (url) {
+		if (url && title) {
 			await axios
 				.get(url)
-				.then(({ data }) => {
+				.then(async ({ data }) => {
 					const $ = cheerio.load(data);
-
 					const el = $(SELECTOR).attr();
-
 					const dataLazy = el && 'data-lazy' in el ? el['data-lazy'] : '';
-					const href = !dataLazy.includes('placeholder') ? dataLazy : undefined;
+					const href = getHref(dataLazy);
 
 					if (href) {
-						console.log({ href });
+						exist++;
+						console.log('\x1b[32m%s\x1b[0m', '[EXIST PHOTO]');
+						await download(title, href);
 					} else {
-						console.log('NO PHOTO');
+						console.log('\x1b[31m%s\x1b[0m', '[NO PHOTO]');
 					}
 				})
-				.catch((e) => console.error(e))
-				.finally(() => {});
+				.catch((e) => {
+					errors++;
+					errorsArray.push(title || url || 'undefined');
+					console.error(e);
+				});
+		} else {
+			console.error({ title, url });
 		}
-
-		sleep();
 	}
 
 	console.log('----- END -----');
+	console.log({ total, exist, errors });
+	if (errors) console.log(errorsArray);
 };
 
 downdloads();
