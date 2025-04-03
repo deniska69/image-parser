3faffaba9984492b346a94c8075f63420acf8d38
deniska69 <gostev.dy@yandex.ru>
2025-04-04 02:55:28 +0700

    complete async loop

--- helpers.ts
+++ helpers.ts
@@ -16,3 +16,12 @@ export const getTitle = (file: string[][], row: number) => {
 export const getUrl = (file: string[][], row: number) => {
 	return file[row]?.length ? file[row][1] : undefined;
 };
+
+export const sleep = (milliseconds = 1000) => {
+	console.info('---sleep---');
+	const date = Date.now();
+	let currentDate = null;
+	do {
+		currentDate = Date.now();
+	} while (currentDate - date < milliseconds);
+};
--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 import axios from 'axios';
 import * as cheerio from 'cheerio';
-import { getUrl, readFile, getTitle } from './helpers.js';
+import { getUrl, readFile, getTitle, sleep } from './helpers.js';
 import { SELECTOR } from './constants.js';
 
 const file = readFile();
@@ -11,38 +11,41 @@ if (!fs.existsSync('images')) fs.mkdirSync('images');
 const downdloads = async () => {
 	console.log('----- START -----');
 
-	const a = 0;
-	const b = 1;
-	const c = 91;
+	const array = [0, 1, 2, 91];
 
-	const n = c;
-	const url = getUrl(file, n);
+	for (const item of array) {
+		const title = getTitle(file, item);
+		const url = getUrl(file, item);
 
-	if (!url) return console.error('url is undefined');
+		console.log('');
+		console.log('title:', title);
+		console.log('url:', url);
 
-	console.log('---------------');
-	console.log(n, getTitle(file, n));
-	console.log(getUrl(file, n));
-	console.log('---------------');
+		if (url) {
+			await axios
+				.get(url)
+				.then(({ data }) => {
+					const $ = cheerio.load(data);
 
-	await axios
-		.get(url)
-		.then(({ data }) => {
-			const $ = cheerio.load(data);
+					const el = $(SELECTOR).attr();
 
-			const el = $(SELECTOR).attr();
+					const dataLazy = el && 'data-lazy' in el ? el['data-lazy'] : '';
+					const href = !dataLazy.includes('placeholder') ? dataLazy : undefined;
 
-			const dataLazy = el && 'data-lazy' in el ? el['data-lazy'] : '';
-			const href = !dataLazy.includes('placeholder') ? dataLazy : undefined;
+					if (href) {
+						console.log({ href });
+					} else {
+						console.log('NO PHOTO');
+					}
+				})
+				.catch((e) => console.error(e))
+				.finally(() => {});
+		}
 
-			if (href) {
-				console.log({ href });
-			} else {
-				console.log('NO PHOTO');
-			}
-		})
-		.catch((e) => console.error(e))
-		.finally(() => console.log('----- END -----'));
+		sleep();
+	}
+
+	console.log('----- END -----');
 };
 
 downdloads();
